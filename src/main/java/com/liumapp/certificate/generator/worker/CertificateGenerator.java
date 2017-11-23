70b1df8e791d9e65ce2bfc73bff767e7af877b93
liumapp <liumapp.com@gmail.com>
2017-11-23 16:36:23 +0800

    certificate generator done

--- src/main/java/com/liumapp/certificate/generator/worker/CertificateGenerator.java
+++ src/main/java/com/liumapp/certificate/generator/worker/CertificateGenerator.java
@@ -9,6 +9,9 @@ import com.liumapp.pattern.certificate.PersonalPattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
+import java.time.temporal.ChronoUnit;
+
 /**
  * 证书生成
  * Created by liumapp on 11/20/17.
@@ -27,7 +30,20 @@ public class CertificateGenerator extends StandReadyWorker {
             PersonalPattern personalPattern = PersonalPattern.parse(s);
             Resource resource = Resource.from(params.getKeyStoreSavePath() + "/" + personalPattern.getKeystore());
             KeyStoreAdapter keyStoreAdapter = KeyTools.keyStoreFrom(resource , personalPattern.getStorepass());
-            
+            keyStoreAdapter.newKeyPair()
+                           .keyLength(personalPattern.getKeysize())
+                           .generateWithCertificate()
+                           .withValidity(1 , ChronoUnit.YEARS)
+                           .withDistinguishName()
+                           .commonName(personalPattern.getName())
+                           .state(personalPattern.getCity())
+                           .locality(personalPattern.getProvince())
+                           .country(personalPattern.getCountry())
+                           .build()
+                           .createInKeyStore(personalPattern.getAlias() , personalPattern.getCertPassword());
+            FileOutputStream out = new FileOutputStream(params.getKeyStoreSavePath() + "/" + personalPattern.getKeystore());
+            keyStoreAdapter.writeTo(out);
+            out.close();
             return "success";
         } catch (Exception e) {
             e.printStackTrace();
