cda5717b811a11fa3dd1cb7d24d86cdecb97616b
liumapp <liumapp.com@gmail.com>
2017-11-23 09:48:14 +0800

    make keystore interface back

--- src/main/java/com/liumapp/certificate/generator/worker/KeyStoreGenerator.java
+++ src/main/java/com/liumapp/certificate/generator/worker/KeyStoreGenerator.java
@@ -3,12 +3,11 @@ package com.liumapp.certificate.generator.worker;
 import com.liumapp.DNSQueen.worker.ready.StandReadyWorker;
 import com.liumapp.certificate.generator.config.Params;
 import com.liumapp.certificate.generator.security.PasswordNeeded;
-import com.liumapp.pattern.SocketPattern;
-import com.liumapp.pattern.exception.PatternPropertiesNumberNotEnough;
-import com.liumapp.pattern.exception.WrongType;
 import com.liumapp.pattern.keystore.KeyStorePattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
 
 /**
  * Created by liumapp on 11/21/17.
@@ -25,16 +24,22 @@ class KeyStoreGenerator extends StandReadyWorker implements PasswordNeeded {
     public String doWhatYouShouldDo(String whatQueenSays) {
         try {
             KeyStorePattern keyStorePattern = KeyStorePattern.parse(whatQueenSays);
+            if (!chkPassword(keyStorePattern)) {
+                return null;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-
         return null;
     }
 
     @Override
-    public boolean chkPassword(SocketPattern socketPattern) {
+    public boolean chkPassword(KeyStorePattern keyStorePattern) {
+
         return false;
     }
+
+
 }
