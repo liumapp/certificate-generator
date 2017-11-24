2dd692864dbd06d7b6304c6849dd7e8be962748f
liumapp <liumapp.com@gmail.com>
2017-11-24 11:48:12 +0800

    证书导出工人的pattern

--- src/main/java/com/liumapp/certificate/generator/worker/CertificateExporter.java
+++ src/main/java/com/liumapp/certificate/generator/worker/CertificateExporter.java
@@ -1,6 +1,7 @@
 package com.liumapp.certificate.generator.worker;
 
 import com.liumapp.DNSQueen.worker.ready.StandReadyWorker;
+import com.liumapp.pattern.certificate.ExporterPattern;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,7 +15,14 @@ public class CertificateExporter extends StandReadyWorker {
 
     @Override
     public String doWhatYouShouldDo(String whatQueenSays) {
-        return null;
+        try {
+            ExporterPattern exporterPattern = ExporterPattern.parse(whatQueenSays);
+            
+            return "success";
+        } catch (Exception e ) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
