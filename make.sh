4b52c26056dfcbe07b953878f0492879437228de
liumapp <liumapp.com@gmail.com>
2017-11-27 17:16:33 +0800

    设置keystore和certificate的存放路径

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 mkdir -p /usr/local/certificateGenerator/
 mkdir -p /usr/local/certificateGenerator/cache
+mkdir -p /usr/local/certificateGenerator/keystore
+mkdir -p /usr/local/certificateGenerator/certificate
 cp ./certificateGenerator.sh /usr/local/certificateGenerator/
 cp ./target/certificate-generator-v1.0.5.jar /usr/local/certificateGenerator/certificate-generator.jar
 
